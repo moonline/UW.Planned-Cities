108116d81a405e90fafa17c97d66e0081b5e4d3a
Tobias Blaser <tobiasblaser@colvarim.ch>
2019-02-27 23:44:33 -0300

    Update summary and references

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir "build"
 pdflatex -output-directory=build "PlannedCommunities.tex"
 bibtex "./build/PlannedCommunities.aux"
 pdflatex -output-directory=build "PlannedCommunities.tex"
